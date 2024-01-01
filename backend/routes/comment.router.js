32655ee788d6aede5a830c8165e0e2afe42877d8
theshrish46 <theshrish46@gmail.com>
2024-01-01 23:25:22 +0530

    refactored the code

--- backend/routes/comment.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { commentPost, likedUser } from "../controllers/comment.controller.js";
-
-const commentRouter = express.Router();
-
-commentRouter.post("/comment/:id", commentPost);
-commentRouter.post("/like/:id", likedUser);
-
-export { commentRouter };
--- frontend/src/app/api/simple/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-export async function GET(request: NextRequest, response: NextResponse) {
-    return Response.json({ msg: 'ok' })
-}
-
-export async function POST() {
-    const res = await fetch()
-}
