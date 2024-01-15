6e6017f9eb69fc7368a108f35fdcc7a785377966
theshrish46 <theshrish46@gmail.com>
2024-01-15 21:13:18 +0530

    fixed client bug

---
+++ components/site-components/NavBarComponent.tsx
@@ -0,0 +1,61 @@
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "../ui/button";
+import Link from "next/link";
+import { LogOutButton } from "./log-out-button";
+import { ModeToggle } from "../mode-toggle";
+import MobileNav from "./MobileNav";
+import { Icons } from "./Icons";
+
+export const NavBarComponent = ({ user }: { user: any }) => {
+  return (
+    <div>
+      <div className="border-b border-gray-200 dark:border-gray-800">
+        <div className="flex h-16 items-center">
+          <div className="ml-4 flex lg:ml-0 justify-between w-full">
+            <Link
+              href={"/"}
+              className="flex justify-center items-center gap-x-1 md:gap-x-3 hover:text-blue-700 transition-all duration-150"
+            >
+              <Icons.logo size={10} />
+              <p className="text-sm font-bold">classnote</p>
+            </Link>
+            <div className="text-2xl">{user?.name}</div>
+            <div className="flex justify-center items-center gap-x-2 md:hidden">
+              <ModeToggle />
+              <MobileNav />
+            </div>
+            <div className="hidden md:flex justify-center items-center gap-x-3">
+              <ModeToggle />
+              <Link
+                href={"/write"}
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "font-semibold text-lg"
+                )}
+              >
+                Write
+              </Link>
+              {user ? (
+                <>
+                  <LogOutButton />
+                </>
+              ) : (
+                <Link
+                  href={"/auth/login"}
+                  className={cn(
+                    buttonVariants({
+                      size: "sm",
+                      variant: "link",
+                    })
+                  )}
+                >
+                  Login
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/site-components/Navbar.tsx
+++ components/site-components/Navbar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
@@ -8,10 +6,11 @@ import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "../ui/button";
 import MobileNav from "./MobileNav";
 import { LogOutButton } from "./log-out-button";
-import { currentClientUser } from "@/hooks/use-client-user";
+import { currentServerUser } from "@/hooks/use-server-user";
+import { NavBarComponent } from "./NavBarComponent";
 
 const Navbar = async () => {
-  const user = await currentClientUser();
+  const user = await currentServerUser();
   if (!user) {
     console.log("No user presentF");
   }
@@ -20,53 +19,7 @@ const Navbar = async () => {
     <div className="bg-white dark:bg-black sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white dark:bg-black">
         <MaxWidthWrapper>
-          <div className="border-b border-gray-200 dark:border-gray-800">
-            <div className="flex h-16 items-center">
-              <div className="ml-4 flex lg:ml-0 justify-between w-full">
-                <Link
-                  href={"/"}
-                  className="flex justify-center items-center gap-x-1 md:gap-x-3 hover:text-blue-700 transition-all duration-150"
-                >
-                  <Icons.logo size={10} />
-                  <p className="text-sm font-bold">classnote</p>
-                </Link>
-                <div className="text-2xl">{user?.name}</div>
-                <div className="flex justify-center items-center gap-x-2 md:hidden">
-                  <ModeToggle />
-                  <MobileNav />
-                </div>
-                <div className="hidden md:flex justify-center items-center gap-x-3">
-                  <ModeToggle />
-                  <Link
-                    href={"/write"}
-                    className={cn(
-                      buttonVariants({ variant: "link" }),
-                      "font-semibold text-lg"
-                    )}
-                  >
-                    Write
-                  </Link>
-                  {user ? (
-                    <>
-                      <LogOutButton />
-                    </>
-                  ) : (
-                    <Link
-                      href={"/auth/login"}
-                      className={cn(
-                        buttonVariants({
-                          size: "sm",
-                          variant: "link",
-                        })
-                      )}
-                    >
-                      Login
-                    </Link>
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
+          <NavBarComponent user={user} />
         </MaxWidthWrapper>
       </header>
     </div>
